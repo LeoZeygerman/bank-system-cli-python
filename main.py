290d924e89f0a79d36f239d435efa95b66161c7e
BBYBOI <zeygermanleo@gmail.com>
2026-05-25 23:00:56 +0500

    add models and json funcs

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ while True:
         number = int(input('Введите номер телефона: '))
         login = input('Введите ваш логин: ')
         password = input('Введите пароль: ')
+        user = {
+            'name': name,
+            'last_name': last_name,
+            'number': number,
+            'login': login,
+            'password': password
+        }
+        data.append(user)
+        save_data(data)
         break
     
     else:
--- models.py
+++ models.py
@@ -1,2 +1,12 @@
 class Bank:
-    def __init__ (self)
+    def __init__ (self, name, last_name, number, login, password):
+        self.name = name
+        self.last_name = last_name
+        self.number = number
+        self.login = login
+        self.password = password
+        
+    def get_info(self):
+        print(f'Имя: {self.name} | Фамилия: {self.last_name} | Номер телефона: {self.number} | Логин: {self.login} | Пароль: {self.password}')
+    
+    
