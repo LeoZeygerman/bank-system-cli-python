5b69dabcf7333940483c1a8a98ec43c81c5717c0
BBYBOI <zeygermanleo@gmail.com>
2026-05-25 23:05:42 +0500

    registration ready

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from storage import save_data, load_data
+from models import Bank
 
 while True:
-    data = load_data()
-    if len(data) == 0:
-        print('Регистрация')
-        name = input('Введите имя: ')
-        last_name = input('Введите фамилию: ')
-        number = int(input('Введите номер телефона: '))
-        login = input('Введите ваш логин: ')
-        password = input('Введите пароль: ')
-        user = {
-            'name': name,
-            'last_name': last_name,
-            'number': number,
-            'login': login,
-            'password': password
-        }
-        data.append(user)
-        save_data(data)
-        break
-    
-    else:
-        print('Войти')
-        break
+    try:
+        data = load_data()
+        if len(data) == 0:
+            print('Регистрация')
+            name = input('Введите имя: ')
+            last_name = input('Введите фамилию: ')
+            number = int(input('Введите номер телефона: '))
+            login = input('Введите ваш логин: ')
+            password = input('Введите пароль: ')
+            user = {
+                'name': name,
+                'last_name': last_name,
+                'number': number,
+                'login': login,
+                'password': password
+            }
+            data.append(user)
+            save_data(data)
+            user_object = Bank(name, last_name, number, login, password)
+            user_object.get_info()
+            break
+        
+        else:
+            print('Войти')
+            break
+    except ValueError:
+        print('Ошибка!')
